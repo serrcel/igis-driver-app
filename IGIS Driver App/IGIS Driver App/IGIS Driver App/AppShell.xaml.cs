647ca7c7ff61f59b06a43a75c1b33962cefce6a5
Соловьев Роман <serrcel@gmail.com>
2022-10-26 19:36:32 +0400

    Created new project of Xamarin app

---
+++ IGIS Driver App/IGIS Driver App/IGIS Driver App/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using IGIS_Driver_App.ViewModels;
+using IGIS_Driver_App.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace IGIS_Driver_App
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ IGIS Driver App/IGIS Driver App/IGIS Driver App/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using IGIS_Driver_App.Models;
+using IGIS_Driver_App.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IGIS_Driver_App.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
