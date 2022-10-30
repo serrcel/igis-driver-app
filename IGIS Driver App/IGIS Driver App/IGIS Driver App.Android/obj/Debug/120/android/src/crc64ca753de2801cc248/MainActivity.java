018c40047c1aa5070e6292122190ee76f940b72a
Артём Лукьянов <a-lukyanov@bk.ru>
2022-10-30 14:25:31 +0400

    Created APIControll class with minimal required functionality for pull API data

--- IGIS Driver App/IGIS Driver App/IGIS Driver App.Android/obj/Debug/120/android/src/crc64ca753de2801cc248/MainActivity.java
+++ IGIS Driver App/IGIS Driver App/IGIS Driver App.Android/obj/Debug/120/android/src/crc64ca753de2801cc248/MainActivity.java
@@ -20,18 +20,16 @@ public class MainActivity
 	public MainActivity ()
 	{
 		super ();
-		if (getClass () == MainActivity.class) {
+		if (getClass () == MainActivity.class)
 			mono.android.TypeManager.Activate ("IGIS_Driver_App.Droid.MainActivity, IGIS Driver App.Android", "", this, new java.lang.Object[] {  });
-		}
 	}
 
 
 	public MainActivity (int p0)
 	{
 		super (p0);
-		if (getClass () == MainActivity.class) {
+		if (getClass () == MainActivity.class)
 			mono.android.TypeManager.Activate ("IGIS_Driver_App.Droid.MainActivity, IGIS Driver App.Android", "System.Int32, mscorlib", this, new java.lang.Object[] { p0 });
-		}
 	}
 
 
---
+++ IGIS Driver App/IGIS Driver App/IGIS Driver App/Models/Driver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net;
+using System.Net.Http.Headers;
+using System.IO;
+
+namespace IGIS_Driver_App.Models
+{
+    public class APIControll
+    {
+        public HttpWebRequest request { get; private set; }
+        public string response { get; private set; }
+        public void GetRequest(string httpAdres)
+        {
+            request.Method = "GET";
+            request = WebRequest.CreateHttp(httpAdres);
+
+            try
+            {
+                HttpWebResponse httpWebResponse = (HttpWebResponse)request.GetResponse();
+                var stream = httpWebResponse.GetResponseStream();
+                if (stream != null)
+                    response = new StreamReader(stream).ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+    public class Driver
+    {
+        
+    }
+}
